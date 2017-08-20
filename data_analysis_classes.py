39440296c84cec37bdebbd96f4f57fd235984dfe
zb5003 <znb5003@gmail.com>
2017-08-20 14:19:29 -0500

    Finished the Levenberg-Marquardt class, but it still needs to be tested.

--- data_analysis_classes.py
+++ data_analysis_classes.py
@@ -1,4 +1,5 @@
 import scipy as sp
+from scipy import linalg as la
 import sympy as sy
 import sympy.utilities.lambdify as lambdify
 import Assorted_Statistics as ass
@@ -8,30 +9,148 @@ class model_construct:
     def __init__(self, model, parameters, indep_var):
         """
         
-        :param model: sympy model
-        :param parameters: List of sy.Symbol
-        :param indep_var:
+        :param model: The model given in terms of sympy functions and symbols.
+        :param parameters: List of sy.Symbols representing the model parameters.
+        :param indep_var: List of sy.Symbols representing the model independet variables.
         """
         self.model = model
         self.parameters = parameters
         self.indep_var = indep_var
 
-        self.model_grad = sy.diff(model, *parameters)
+        self.model_grad = [sy.diff(model, i) for i in parameters]
+        self.model_hessian = [[sy.diff(model, i, j) for j in parameters] for i in parameters]
 
     def model_num(self, args):
         """
-        
-        :param args: 
-        :return: 
+        Evaluate the model for numeric values of the parameters and independent variables.
+        :param args: A list structured as [[parameters], [indep_var]].
+        :return: Numerical evaluation of the model.
         """
         func = lambdify([self.parameters, self.indep_var], self.model)
         return func(*args)
 
     def model_grad_num(self, args):
         """
-        
-        :param args: 
-        :return: 
+        Evaluate the model gradient for numeric values of the parameters and independent variables.
+        :param args: A list structured as [[parameters], [indep_var]].
+        :return: Numerical evaluation of the model gradient.
         """
         func = lambdify([self.parameters, self.indep_var], self.model_grad)
         return func(*args)
+
+    def model_hessian_num(self, args):
+        """
+        Evaluate the model Hessian for numeric values of the parameters and independent variables.
+        Technically this is not the actual Hessian, but the one that is typically used for nonlinear fits.
+        This is explained in William Press, Numerical Recipies (Third Edition), section 15.5.1 page 801.
+        :param args: A list structured as [[parameters], [indep_var]].
+        :return: Numerical evaluation of the model Hessian.
+        """
+        func = lambdify([self.parameters, self.indep_var], self.model_hessian)
+        return func(*args)
+
+class levenberg_marquardt:
+
+    def __init__(self, a_model_obj, parameter_guess, the_data, sigs):
+        """
+        Initialize object for optimizing the parameters of a model via the Levenberg-Marquardt method.
+        :param a_model_obj: An instance of model_construct representing the model to be fit.
+        :param parameter_guess: An array containing the initial guess for the parameters.
+        :param the_data: An array containing the measured date.
+        :param sigs: the measurement uncertainty for each measured value.
+        """
+        self.a_model_obj = a_model_obj
+        self.parameter_guess = parameter_guess
+        self.the_data = the_data
+        self.sigs = sigs
+
+        self.theory = self.func_eval(self.a_model_obj.model_num, self.current_parameters)
+        self.cs = ass.chi_squared(self.the_data, self.theory, self.sigs)
+
+        self.n = len(the_data)
+        self.cs_error = 0.001 * self.n
+        self.occurences = 5
+        self.max_it = 1000
+        self.current_it = [0]
+
+        self.lam = 0.001
+        self.current_parameters = parameter_guess
+
+        self.beta = sum(self.func_eval(-self.a_model_obj.model_grad_num / 2, self.current_parameters))
+        self.alpha = sum(self.func_eval(self.a_model_obj.model_hessian_num / 2, self.current_parameters))
+        self.alpha_prime = self.alpha + self.lam * sp.diag(sp.diag(self.alpha))
+
+    def func_eval(self, func, param):
+        """
+        Evaluate a function for every measured data point.
+        :param func: The function to be evaluated.
+        :param param: The parameters to evaluate the function with.
+        :return: An array containing the function evaluated for each measured value.
+        """
+        evaluated = []
+        for i in self.the_data:
+            evaluated.append(func([[param], [i]]))
+
+        return evaluated
+
+    def lev_mar_update(self, test_cs, test_param):
+        """
+        Update self.theory (and all the attributes that depend on it) according to the Levenberg-Marquardt method.
+        :param test_cs: The test update for self.cs.
+        :param test_param: The test parameters.
+        :return: None.
+        """
+        if test_cs < self.cs:
+            self.current_parameters = test_param
+            self.lam = 0.1 * self.lam
+        else:
+            self.lam = 10 * self.lam
+
+        return None
+
+    def lev_mar_step(self):
+        """
+        Update the current_parameter (not the attribute) via the Levenberg-Marquardt method.
+        :return: The updated parameters.
+        """
+        return self.current_parameters + la.inv(self.alpha_prime).dot(self.beta)
+
+    def check_stop(self):
+        """
+        Prevent the algorithm from never ending.
+        :return: None.
+        """
+        self.current_it[0] = self.current_it[0] + 1
+        if self.current_it[0] > self.max_it:
+            raise Exception("TOO MANY ITERATIONS!!!")
+
+        return None
+
+    def lev_mar_run(self):
+        """
+        Carry out the Levenberg-Marquardt method to optimize the parameters for the given model.
+        :return: None.
+        """
+        checks = []
+        for i in range(self.occurences):
+            param_new = self.lev_mar_step()
+            theory_new = self.func_eval(self.a_model_obj.model_num, param_new)
+            cs_new = ass.chi_squared(self.the_data, theory_new, self.sigs)
+
+            d_cs = cs_new - self.cs
+            checks.append(d_cs)
+
+            self.lev_mar_update(cs_new, param_new)
+
+        while not [checks[-self.occurences:] < self.cs_error]:
+            param_new = self.lev_mar_step()
+            theory_new = self.func_eval(self.a_model_obj.model_num, param_new)
+            cs_new = ass.chi_squared(self.the_data, theory_new, self.sigs)
+
+            d_cs = cs_new - self.cs
+            checks.append(d_cs)
+
+            self.lev_mar_update(cs_new, param_new)
+            self.check_stop()
+
+        return None
--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import scipy as sp
 from Assorted_Statistics import *
+from data_analysis_classes import *
 import numpy.random as rnd
+import sympy as sy
 
-a = rnd.nor
+x = sy.Symbol('x')
+y = sy.Symbol('y')
+z = sy.Symbol('z')
+f = y * x**1 + sy.sin(y) * z**3
+
+model = model_construct(f, [y, z], [x])
+# print(sy.diff(f, y, z))
+print(model.model, model.model_grad)
+print(model.model_num([[2, 2], [0]]))
+print(model.model_grad_num([[2, 2], [0]]))
+print(model.model_hessian)
